c4f00636edf8dbb0717a2263cbb1c86b242cd4e2
Niels Ormerod <niels.ormerod@mediabirds.nl>
2023-03-08 10:24:40 -0400

    update email sender

--- config/plugins.js
+++ config/plugins.js
@@ -25,9 +25,9 @@ module.exports = ({ env }) => ({
           apiKey: env('SENDGRID_API_KEY'),
         },
         settings: {
-          defaultFrom: 'hello@mediabirds.nl',
-          defaultReplyTo: 'hello@mediabirds.nl',
-          testAddress: 'niels.ormerod@mediabirds.nl',
+          defaultFrom: 'info@yoepie.nl',
+          defaultReplyTo: 'info@yoepie.nl',
+          testAddress: 'dev@mediabirds.nl',
         },
       },
     },
