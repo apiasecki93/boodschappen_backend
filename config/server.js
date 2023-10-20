aacaba457d67ab215f80caeb7bae2ff190f85935
apiasecki93@gmail.com <apiasecki93@gmail.com>
2023-10-20 14:41:56 +0200

    switched to port 1338

--- config/server.js
+++ config/server.js
@@ -1,8 +1,8 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', 1338),
   app: {
     keys: env.array("APP_KEYS"),
   },
-  url: env('', 'http://localhost:1337'),
+  url: env('', 'http://localhost:1338'),
 });
